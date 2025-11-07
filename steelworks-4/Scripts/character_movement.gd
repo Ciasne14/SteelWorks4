5d99a3ade6e288d04a056cbdbb2641cf7c14922d
Mateusz0lszewski <mateuszolszewski@opoczta.pl>
2025-11-07 21:45:36 +0100

    menu

--- steelworks-4/Scripts/character_movement.gd
+++ steelworks-4/Scripts/character_movement.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
@@ -15,9 +15,22 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
+	if horizontal_direction > 0:
+		animated_sprite.flip_h = 1
+	
+	elif horizontal_direction < 0:
+		animated_sprite.flip_h = 0
+		
 	if vertical_direction:
 		velocity.y = vertical_direction * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+	if horizontal_direction:
+		velocity.x = horizontal_direction * SPEED
+		animated_sprite.animation = "walking"
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animated_sprite.animation = "default"
 
 	move_and_slide()
--- steelworks-4/Scripts/tent_balloon.gd
+++ steelworks-4/Scripts/tent_balloon.gd
@@ -3,4 +3,3 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	get_tree().change_scene_to_file(minigame)
-	
