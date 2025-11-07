ff65e101907149bf9ac6929da80805296d05d9dc
Mateusz0lszewski <mateuszolszewski@opoczta.pl>
2025-11-07 20:25:26 +0100

    kontroler gracza

--- steelworks-4/Scripts/tent_balloon.gd
+++ steelworks-4/Scripts/tent_balloon.gd
@@ -2,8 +2,5 @@ extends Area2D
 @onready var minigame = "res://Scenes/balloon_poper.tscn"
 
 func _on_body_entered(body: Node2D) -> void:
-	pass
-
-
-func _on_mouse_entered() -> void:
 	get_tree().change_scene_to_file(minigame)
+	
