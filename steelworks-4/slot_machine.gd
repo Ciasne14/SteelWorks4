d1d84cceca11ebc2e16b644baf93f652ef93816a
LAPTOP-LENOVO\pawel <pawelek.tokarczyk@gmail.com>
2025-11-08 13:00:22 +0100

    slot upd

--- steelworks-4/slot_machine.gd
+++ steelworks-4/slot_machine.gd
@@ -7,6 +7,13 @@ extends Node  # Jeśli masz Node2D jako główny węzeł
 @export var stop_delay = 0.5 # Opóźnienie przed zatrzymaniem bębna
 @export var tickets = 100
 
+@export var roll_particle: PackedScene
+
+var sprites : Array = []
+
+# Ścieżka do folderu ze sprite'ami
+var folder_path : String = "res://Assets/Colored/"
+
 var bet = 0
 var reels = [] # Lista bębnów
 var spinning = false # Czy bębny kręcą się?
@@ -16,12 +23,15 @@ var btn5modulate
 # Funkcja inicjująca
 func _ready():
 	# Inicjalizuj bębny
+	load_sprites_from_folder(folder_path) 
 	btn5modulate = $btn5.modulate
 	for j in range(row_reels):
 		for i in range(num_reels):
 			var reel = Sprite2D.new()
-			reel.texture = load("res://Assets/Dice/"+symbols[randi() % symbols.size()]) # Wybór losowego symbolu na start
+			reel.texture = sprites[randi_range(1,sprites.size()-1)] # Wybór losowego symbolu na start
+			reel.scale = Vector2(.8,.8)
 			reel.position = Vector2(100 * i, 100 * j) # Przesuń bębny na ekranie
+			
 			add_child(reel)
 			reels.append(reel)
 			reel_positions.append(0)  # Początkowa pozycja bębna
@@ -41,7 +51,10 @@ func _on_spin_pressed():
 	for i in range(num_reels*row_reels):
 		reel_positions[i] = 0
 		var reel = reels[i]
-		reel.texture = load("res://Assets/Dice/"+symbols[randi() % symbols.size()])
+		reel.texture = sprites[randi_range(1,sprites.size()-1)]
+		var explosion = roll_particle.instantiate()
+		explosion.global_position = reel.position
+		get_tree().current_scene.add_child(explosion)
 		# Uruchom animację obrotu
 		start_reel_spin(i)
 
@@ -53,13 +66,13 @@ func start_reel_spin(reel_index):
 func stop_reel_spin(reel_index):
 	var reel = reels[reel_index]
 	
-	reel.texture = load("res://Assets/Dice/"+symbols[randi() % symbols.size()])  # Zmiana na losowy symbol po zatrzymaniu
+	reel.texture = sprites[randi_range(1,sprites.size())-1]  # Zmiana na losowy symbol po zatrzymaniu
 	if reel_index == num_reels - 1:
 		spinning = false
-		check_result()
+		#check_result()
 
 # Funkcja do sprawdzenia wyniku
-func check_result():
+func scheck_result():
 	var result = []
 	for reel in reels:
 		if reel.texture != null:
@@ -72,6 +85,18 @@ func check_result():
 			result.append("Brak tekstury")
 	print("Wynik: " + str(result))
 
+func load_sprites_from_folder(path: String):
+	var dir = DirAccess.open(path)  # Używamy DirAccess do otwarcia folderu
+	if dir:
+		dir.list_dir_begin()  # Zaczynamy przeglądanie folderu
+		var file_name = dir.get_next()
+		while file_name != "":
+			# Sprawdzamy, czy to jest plik z obrazkiem (np. .png)
+			if file_name.ends_with(".png"):  # Możesz dostosować rozszerzenie
+				var texture = load(path + file_name)  # Ładujemy teksturę
+				sprites.append(texture)  # Dodajemy do tablicy
+			file_name = dir.get_next()  # Przechodzimy do następnego pliku
+		dir.list_dir_end()  # Kończymy przeglądanie folderu
 
 func _on_button_pressed() -> void:
 	_on_spin_pressed()
