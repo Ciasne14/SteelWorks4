f0774100bb627c0be82287a554e995393ad2b338
Mateusz0lszewski <mateuszolszewski@opoczta.pl>
2025-11-08 02:40:08 +0100

    menu 3.0

--- steelworks-4/Scripts/back.gd
+++ steelworks-4/Scripts/back.gd
@@ -3,5 +3,6 @@ extends Button
 @onready var target_scene = "res://Scenes/main_menu.tscn"
 
 
-func _on_button_down() -> void:
-	get_tree().change_scene_to_file(target_scene)
+
+func _on_pressed() -> void:
+		get_tree().change_scene_to_file(target_scene)
