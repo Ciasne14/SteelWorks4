57331fcc632804ec34cbf84f1780b47c2bf18c5c
Mateusz0lszewski <mateuszolszewski@opoczta.pl>
2025-11-08 01:34:58 +0100

    menu 2.0

--- steelworks-4/Scripts/character_movement.gd
+++ steelworks-4/Scripts/character_movement.gd
@@ -1,36 +1,35 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
+const SPEED := 200.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var horizontal_direction := Input.get_axis("left", "right")
-	var vertical_direction := Input.get_axis("up", "down")
-	
-	if horizontal_direction:
-		velocity.x = horizontal_direction * SPEED
+	var h := Input.get_axis("left", "right")
+	var v := Input.get_axis("up", "down")
+
+
+	if h != 0.0:
+		velocity.x = h * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	if horizontal_direction > 0:
-		animated_sprite.flip_h = 1
-	
-	elif horizontal_direction < 0:
-		animated_sprite.flip_h = 0
-		
-	if vertical_direction:
-		velocity.y = vertical_direction * SPEED
+
+	if v != 0.0:
+		velocity.y = v * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-		
-	if horizontal_direction:
-		velocity.x = horizontal_direction * SPEED
+
+	if v > 0.0:
+		animated_sprite.animation = "down"
+	elif v < 0.0:
+		animated_sprite.animation = "up"
+	elif h != 0.0:
 		animated_sprite.animation = "walking"
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animated_sprite.animation = "default"
+
+	if h > 0.0:
+		animated_sprite.flip_h = true
+	elif h < 0.0:
+		animated_sprite.flip_h = false
 
 	move_and_slide()
---
+++ steelworks-4/Scripts/transition.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@export var target_scene: PackedScene
+@export var exits_game: bool = false
+
+#...
+func _on_body_entered(body: Node2D) -> void:
+	if exits_game:
+		get_tree().quit()
+	else:
+		get_tree().change_scene_to_packed(target_scene)
