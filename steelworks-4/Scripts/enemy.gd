1e4206aeea21a584c4475334f4e8e0c4d0917106
Mateusz0lszewski <mateuszolszewski@opoczta.pl>
2025-11-08 12:05:48 +0100

    mapa

--- steelworks-4/Scripts/enemy.gd
+++ steelworks-4/Scripts/enemy.gd
@@ -1,23 +1,51 @@
 extends CharacterBody2D
 
+<<<<<<< Updated upstream
 var speed = 150
 enum States {Chase, Idle}
 var State
+=======
+@onready var wander_controller = $WanderController
+var speed = 150
+enum States {Chase, Idle, Wander}
+var State = States.Idle
+>>>>>>> Stashed changes
 var player = null
 
 func _physics_process(delta: float) -> void:
 	if State == States.Chase:
+<<<<<<< Updated upstream
 		position += (player.position - position)/speed
+=======
+		var direction = (player.position - position).normalized()
+		velocity = direction * speed
+		move_and_slide()
+>>>>>>> Stashed changes
 		
 		if (player.position.x - position.x) < 0:
 			$AnimatedSprite2D.flip_h = 1
 		else:
 			$AnimatedSprite2D.flip_h = 0
+<<<<<<< Updated upstream
 			
 	move_and_slide() 
 	
 	if State == States.Idle:
 		$Timer.start()
+=======
+	
+	if State == States.Idle and wander_controller.get_time_left()==0:
+		$WanderController/Timer.start()
+		
+		
+	if State == States.Wander:
+		var direction = (wander_controller.target_position - position).normalized()
+		velocity = direction * speed
+		move_and_slide()
+
+		if position.distance_to(wander_controller.target_position) < 4:
+			wander_controller.update_target_position()
+>>>>>>> Stashed changes
 		 
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	player = body
@@ -27,3 +55,7 @@ func _on_detection_area_body_entered(body: Node2D) -> void:
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	player = null
 	State = States.Idle
+<<<<<<< Updated upstream
+=======
+	
+>>>>>>> Stashed changes
--- steelworks-4/Scripts/wander_controller.gd
+++ steelworks-4/Scripts/wander_controller.gd
@@ -3,11 +3,30 @@ extends Node2D
 @onready var start_position = global_position
 @onready var target_position
 @onready var range = 32
+<<<<<<< Updated upstream
+=======
+@onready var timer = $Timer
+
+func _ready():
+	update_target_position()
+	timer.start()
+>>>>>>> Stashed changes
 
 func update_target_position():
 	var target_vector = Vector2(randi_range(-range, range), randi_range(-range, range))
 	target_position = start_position + target_vector
 
+<<<<<<< Updated upstream
 
 func _on_timer_timeout() -> void:
 	update_target_position()
+=======
+func set_timer(duration):
+	timer.start(duration)
+
+func _on_timer_timeout() -> void:
+	update_target_position()
+
+func get_time_left():
+	return timer.time_left
+>>>>>>> Stashed changes
