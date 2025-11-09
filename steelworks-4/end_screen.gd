a84e75e93c2116ea43e81881e6b8429c6064f7e2
LAPTOP-LENOVO\pawel <pawelek.tokarczyk@gmail.com>
2025-11-09 04:52:22 +0100

    end_screen

---
+++ steelworks-4/end_screen.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_again_button_down() -> void:
+	pass # Replace with function body.
+
+
+func _on_menu_button_down() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_button_down() -> void:
+	pass # Replace with function body.
